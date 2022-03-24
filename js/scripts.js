d4bab6243e2fa7ec1cf12a5bc5b5f50c46544ff8
neryspetrou <101205758+neryspetrou@users.noreply.github.com>
2022-03-24 17:48:43 +0000

    js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello and welcome to my portfolio website!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
